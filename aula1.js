3c37f66f4439b4118698274ceaf8b081c0c004e1
Kemily <pontes.kemily@escola.pr.gov.br>
2026-03-09 08:54:33 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,10 @@
+let nome = "Kemily"
+let altura = 1.54
+let idade = 16
+let hobby = "Jogar volêi"
+let gênerodefilmefavorito = "Terror e romance"
+let estilodemusica = "kpoop, pop, acústico"
+let corfavorita = "roxo"
+let florfavorita = "tulipa"
+ 
+console.log("Olá, meu nome é " + nome + " tenho " + idade + " anos " + " tenho " + altura + " de altura " + " meu hobby é " + hobby + " meu gênero de filme favorito é " + gênerodefilmefavorito + " gosto de escutar " + estilodemusica + " minha cor favorita é " + corfavorita + " minha flor favorita é " + florfavorita)
