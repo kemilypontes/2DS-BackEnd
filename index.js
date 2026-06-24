1273d579f81eabb65d411ed8cc5f8bf4486a84f3
Kemily <pontes.kemily@escola.pr.gov.br>
2026-06-24 09:45:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,30 @@ app.post("/clientes", (req, res) => {
 })
 
 app.get("/clientes", (req, res) => {
-    try{
-        // abrir arquivo 
-        const bd =  JSON.parse(fs.readFileSync("bd.json", "utf8"))
+    try {
+        const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
         res.status(200).json({resposta: bd})
-    } catch (erro)  {
+    } catch (erro) {
         res.status(500).json({erro: erro.message})
+    }
+})
+app.delete("/clientes/:cpf", (req, res) => {
+    //pegar o cpf da rota
+    const cpf = req.params.cpf
+        try{
+        //abrir o banco de dados
+        const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+         //encontrar o indice do cliente a ser excluido
+        const indiceClientes = bd.findIndex((cliente) => cliente.cpf == cpf)
+        //remover o indice da lista
+        if(indiceClientes == -1){
+            return res.status(484).json({erro: "O cliente não existe"})
+        }
+        bd.splice(indiceClientes, 1)
+        //dar uma resposta para o cliente
+        res.status(200).json({resposta:"Cliente excluido com sucesso"})
+    }catch (error){
+        res.status(500).json({erro: erro.mensageS})
     }
 })
 
@@ -37,4 +55,4 @@ app.listen(port, ()=>{
     console.log("API rodando na porta" + port)
 })
 
-// GET http//loacalhost:3000/clientes
+// GET http://localhost:3000/clientes
